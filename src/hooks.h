52bb1fedb02bc42fdd44972eaf6e6b43fc66a7ea
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-09-17 13:26:32 -0400

    feat: working angle calculation
    cleaned up old, unused code.

--- src/hooks.h
+++ src/hooks.h
@@ -1,66 +1,73 @@
 #pragma once
 
-namespace hooks {
-    //adapted from valhalla combat
-    static void OnArrowCollision(RE::Projectile* a_this, RE::hkpAllCdPointCollector* a_AllCdPointCollector);
-    static void OnMissileCollision(RE::Projectile* a_this, RE::hkpAllCdPointCollector* a_AllCdPointCollector);
-    static inline REL::Relocation<decltype(OnArrowCollision)> _arrowCollision;
-    static inline REL::Relocation<decltype(OnMissileCollision)> _missileCollision;
+class hooks {
+    //logic adapted from valhalla combat, hooks adapted from arrowInterpreter. 
+    public: 
+        static inline void install() {
+            SKSE::log::info("[hooks] attempting hooking projectile collision functions");
 
-    static inline void install() {
-        SKSE::log::info("[hooks] attempting hooking projectile collision functions");
-        REL::Relocation<std::uintptr_t> arrowProjectileVtbl{ RE::VTABLE_ArrowProjectile[0] };
-        REL::Relocation<std::uintptr_t> missileProjectileVtbl{ RE::VTABLE_MissileProjectile[0] };
+            {
+                REL::Relocation<std::uintptr_t> vtable{ RE::VTABLE_ArrowProjectile[0]};
+                _originalArrow = vtable.write_vfunc(0xBD, AddImpactProj);
+            }
 
-        _arrowCollision = arrowProjectileVtbl.write_vfunc(190, OnArrowCollision);
-        _missileCollision = missileProjectileVtbl.write_vfunc(190, OnMissileCollision);
-        SKSE::log::info("[hooks] attempting hooking projectile collision functions");
+            {
+                REL::Relocation<std::uintptr_t> vtable{ RE::VTABLE_MissileProjectile[0]};
+                _originalMissile = vtable.write_vfunc(0xBD, AddImpactMissile);
+            }
+
+            SKSE::log::info("[hooks] attempting hooking projectile collision functions");
+        }
     
-    }
-
-    bool checkBlockAngle(RE::Actor* actor, RE::TESObjectREFR* a_obj) {
-        auto angle = actor->GetHeadingAngle(a_obj->GetAngle(), true);
-        auto* gameSettings = RE::GameSettingCollection::GetSingleton();
-        auto* gmst = gameSettings ? gameSettings->GetSetting("fCombatHitConeAngle") : nullptr;
-        const float fCombatHitConeAngle = gmst->GetFloat();
-        SKSE::log::info("[hooks] angle: {}", angle);
-	    return (angle <= fCombatHitConeAngle);
-    }
-
-    void performProjectileBlock(RE::Actor* blocker, RE::TESObjectREFR* a_obj) {
-        if (!blocker || !a_obj) {
-            return;
+    private: 
+        static bool checkBlockAngle(RE::Actor* actor, RE::TESObjectREFR* a_obj) {
+            auto angle = actor->GetHeadingAngle(a_obj->GetAngle(), true);
+            auto* gameSettings = RE::GameSettingCollection::GetSingleton();
+            auto* gmst = gameSettings ? gameSettings->GetSetting("fCombatHitConeAngle") : nullptr;
+            if (gmst) {
+                const float fCombatHitConeAngle = gmst->GetFloat();
+                SKSE::log::info("[hooks] angle: {}/{}", angle, fCombatHitConeAngle);
+                return (angle <= fCombatHitConeAngle);
+            }
+            
+            return false;
         }
-        if (!checkBlockAngle(blocker, a_obj)) {
-            return;
-        }
-        if (blocker->IsBlocking()) {
-			blocker->NotifyAnimationGraph("BlockHitStart");
-		}
-    }
 
-    void processProjectileCollision(RE::Projectile* a_projectile, RE::hkpAllCdPointCollector* a_AllCdPointCol) {
-        if (a_AllCdPointCol) {
-            for (auto& hit : a_AllCdPointCol->hits) {
-                auto refA = RE::TESHavokUtilities::FindCollidableRef(*hit.rootCollidableA);
-                auto refB = RE::TESHavokUtilities::FindCollidableRef(*hit.rootCollidableB);
-                if (refA && refA->formType == RE::FormType::ActorCharacter) {
-                    performProjectileBlock(refA->As<RE::Actor>(), a_projectile);
-                }
-                if (refB && refB->formType == RE::FormType::ActorCharacter) {
-                    performProjectileBlock(refB->As<RE::Actor>(), a_projectile);
-                }
+        static void performProjectileBlock(RE::Actor* blocker, RE::TESObjectREFR* a_obj) {
+            if (!blocker || !a_obj) {
+                return;
+            }
+            if (!checkBlockAngle(blocker, a_obj)) {
+                return;
+            }
+            if (blocker->IsBlocking()) {
+                blocker->NotifyAnimationGraph("BlockHitStart");
             }
         }
-    }
 
-    void arrowCollisionhook(RE::Projectile* a_this, RE::hkpAllCdPointCollector* a_AllCdPointCollector) {
-        processProjectileCollision(a_this, a_AllCdPointCollector);
-        _arrowCollision(a_this, a_AllCdPointCollector);
-    }
+        static void processProjectileCollision(RE::ArrowProjectile* a_projectile, RE::TESObjectREFR* a_ref) { 
+            // SKSE::log::info("[processProjCollision]");
+            if (!a_projectile || !a_ref) {
+                return;
+            }
+            if (a_ref->formType == RE::FormType::ActorCharacter) {
+                performProjectileBlock(a_ref->As<RE::Actor>(), a_projectile);
+            }
+        }
 
-    void missileCollisionHook(RE::Projectile* a_this, RE::hkpAllCdPointCollector* a_AllCdPointCollector) {
-        processProjectileCollision(a_this, a_AllCdPointCollector);
-        _missileCollision(a_this, a_AllCdPointCollector);
-    }
-}
+        //should natively support all projectile types if I hook this
+        static RE::Projectile::ImpactData* AddImpactProj(RE::ArrowProjectile* a_projectile, RE::TESObjectREFR* a_ref, const RE::NiPoint3& a_targetLoc, const RE::NiPoint3& a_velocity, RE::hkpCollidable* a_collidable, std::int32_t a_arg6, std::uint32_t a_arg7) {
+            // SKSE::log::info("[AddImpactProj]");
+            processProjectileCollision(a_projectile, a_ref);
+            return _originalArrow(a_projectile, a_ref, a_targetLoc, a_velocity, a_collidable, a_arg6, a_arg7);
+        }
+        
+        static RE::Projectile::ImpactData* AddImpactMissile(RE::ArrowProjectile* a_projectile, RE::TESObjectREFR* a_ref, const RE::NiPoint3& a_targetLoc, const RE::NiPoint3& a_velocity, RE::hkpCollidable* a_collidable, std::int32_t a_arg6, std::uint32_t a_arg7) {
+            // SKSE::log::info("[AddImpactMissile]");
+            processProjectileCollision(a_projectile, a_ref);
+            return _originalMissile(a_projectile, a_ref, a_targetLoc, a_velocity, a_collidable, a_arg6, a_arg7);
+        }
+        
+        static inline REL::Relocation<decltype(AddImpactProj)> _originalArrow;
+        static inline REL::Relocation<decltype(AddImpactMissile)> _originalMissile;
+};
