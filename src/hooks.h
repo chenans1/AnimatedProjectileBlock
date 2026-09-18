80e9d3e756dc7dba86bbf1b8701e5915d4120711
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-09-17 23:31:17 -0400

    feat: basic attempt at spell blocking
    largely just tracks spell collision effects. Does not actually work other than visually.

--- src/hooks.h
+++ src/hooks.h
@@ -33,7 +33,8 @@ class hooks {
             SKSE::log::info("[hooks] attempting hooking projectile ApplyProjectileSpell");
 
             auto& trampoline = SKSE::GetTrampoline();
-            originalApply = trampoline.write_call<5>(REL::RelocationID(42943, 44123).address() + REL::Relocate(0x31C, 0x312), ApplyProjectileSpell);
+            // originalApply = trampoline.write_call<5>(REL::RelocationID(42943, 44123).address() + REL::Relocate(0x31C, 0x312), ApplyProjectileSpell);
+            originalApply = trampoline.write_call<5>(REL::Relocation<std::uintptr_t>{ REL::Offset(0x7EC608) }.address(), ApplyProjectileSpell);
             originalSetEffectiveness = SKSE::GetTrampoline().write_call<5>(REL::RelocationID(33763, 34547).address() + REL::Relocate(0x4A3, 0x656), SetEffectiveness);
             SKSE::log::info("[Hooks] originalApply at address: 0x{:X}", originalApply.address());
             SKSE::log::info("[hooks] SetEffectiveness at address: 0x{:X}", originalSetEffectiveness.address());
@@ -74,17 +75,51 @@ class hooks {
         };
 
         static inline std::mutex mutex;
-        static inline std::unordered_map<const RE::Projectile::ImpactData*, Hit> pending;
+        static inline std::vector<Hit> pending;
         static inline thread_local std::optional<Hit> currentHit;
-        // static inline std::atomic<std::uint32_t> loggedCollisions{0};
+        
+        static void recordHit( RE::Projectile* projectile, RE::TESObjectREFR* target, float remainingDamage) {
+            if (!projectile || !target || !std::isfinite(remainingDamage)) {
+                return;
+            }
+            auto* spell = projectile->GetProjectileRuntimeData().spell;
+            if (!spell) {
+                return;
+            }
+            Hit hit{
+                projectile->GetHandle(),
+                target->GetHandle(),
+                spell,
+                std::clamp(remainingDamage, 0.0f, 1.0f)
+            };
 
-        // static std::optional<Hit> TakeHit(RE::Projectile* projectile, RE::Projectile::ImpactData* impact) {
-        //     if (!projectile || !impact) {
-        //         return std::nullopt;
-        //     }
-        //     std::lock_guard lock(mutex);
+            std::lock_guard lock(mutex);
+            pending.push_back(std::move(hit));
+        }
 
-        // }
+        static std::optional<Hit> TakeHit(RE::Projectile* projectile, RE::TESObjectREFR* target) {
+            if (!projectile || !target) {
+                return std::nullopt;
+            }
+            
+            const auto projectileHandle = projectile->GetHandle();
+            const auto targetHandle = target->GetHandle();
+            auto* spell = projectile->GetProjectileRuntimeData().spell;
+            if (!spell) {
+                return std::nullopt;
+            }
+
+            std::lock_guard lock(mutex);
+            for (auto it = pending.begin(); it != pending.end(); ++it) {
+                const Hit& hit = *it;
+                if (hit.projectile == projectileHandle && hit.target == targetHandle && hit.spell == spell) {
+                    Hit result = *it;
+                    pending.erase(it);
+                    return result;
+                }
+            }
+            return std::nullopt;
+        }
 
         //cooldown to not excessively make actors play blockhit animations
         static bool applyCD(RE::Actor* actor) {
@@ -145,23 +180,22 @@ class hooks {
             return false;
         }
 
-        static void performProjectileBlock(RE::Actor* blocker, RE::Projectile* projectile) {
+        static bool performProjectileBlock(RE::Actor* blocker, RE::Projectile* projectile) {
             if (!blocker || !projectile) {
-                return;
+                return false;
             }
 
             if (!checkBlockAngle(blocker, projectile) || !blocker->IsBlocking()) {
-                return;
+                return false;
             }
-
             if (projectile->formType == RE::FormType::ProjectileFlame) {
                 if (!applyCD(blocker)) {
                     // SKSE::log::info("[performProjectileBlock]: flame projectile blocker has CD effect");
-                    return; 
+                    return true; 
                 }
             }
             blocker->NotifyAnimationGraph("BlockHitStart");
-            
+            return true;
         }
 
         static void processProjectileCollision(RE::Projectile* a_projectile, RE::TESObjectREFR* a_ref) { 
@@ -171,7 +205,10 @@ class hooks {
             }
             if (a_ref->formType == RE::FormType::ActorCharacter) {
                 auto* actor = a_ref->As<RE::Actor>();
-                performProjectileBlock(actor, a_projectile);
+                if (performProjectileBlock(actor, a_projectile)) {    
+                    SKSE::log::info("[AddImpactMissile] recorded blocked projectile={}, target={}", static_cast<void*>(a_projectile), static_cast<void*>(a_ref));
+                    recordHit(a_projectile, a_ref, 0.0f);
+                }
             }
         }
 
@@ -208,18 +245,39 @@ class hooks {
         static inline RE::SpellItem* cooldownSpell = nullptr;       // 0x800
         static inline RE::EffectSetting* cooldownEffect = nullptr;  // 0x801
 
-        //called from inside Projectile::ProcessImpact()??
+        //it turns out this function - which applies the spell effects from projectile collision - actually runs before the addimpact() hooks
+        //it also turns out in the same synchronous call, setEffectiveness is called. 
         static void ApplyProjectileSpell(RE::MagicCaster* caster, const RE::NiPoint3* impactPos, RE::Projectile* projectile, RE::TESObjectREFR* target, float arg5, float arg6, std::uint8_t arg7, std::uint8_t arg8) {
-            // HitScope scope(TakeHit(projectile, impact));
-            SKSE::log::info("[ApplyProjectileSpell] projectile={} target={}", static_cast<void*>(projectile), static_cast<void*>(target));
+            // HitScope scope(TakeHit(projectile, target));
+            SKSE::log::info("[ApplyProjectileSpell] ENTER: projectile={} target={} blocked={}", static_cast<void*>(projectile), static_cast<void*>(target), currentHit.has_value());
             originalApply(caster, impactPos, projectile, target, arg5, arg6, arg7, arg8);
+            SKSE::log::info("[ApplyProjectileSpell] EXIT: projectile={} target={} blocked={}", static_cast<void*>(projectile), static_cast<void*>(target), currentHit.has_value());
         }
 
         static void SetEffectiveness(RE::ActiveEffect* effect, float power, bool onlyHostile) {
             originalSetEffectiveness(effect, power, onlyHostile);
-            // if (effect) {
-            //     SKSE::log::info("[SetEffectiveness] magnitude after original={}", effect->magnitude);
-            // }
+            if (!currentHit || !effect) {
+                return;
+            }
+            // Must belong to the spell from the blocked projectile.
+            if (effect->spell != currentHit->spell) {
+                return;
+            }
+            if (!effect->IsCausingHealthDamage()) {
+                return;
+            }
+            //do not reduce damage for targets that aren't blocking
+            auto* victim = effect->GetTargetActor();
+            if (!victim) {
+                return;
+            }
+            if (victim->GetHandle() != currentHit->target) {
+                return;
+            }
+            const float oldMagnitude = effect->magnitude;
+            effect->magnitude *= currentHit->remainingDamage;
+            SKSE::log::info("[SetEffectiveness] blocked spell effect={} magnitude {} -> {}", static_cast<void*>(effect), oldMagnitude, effect->magnitude);
+
         }
 
         static inline REL::Relocation<decltype(ApplyProjectileSpell)> originalApply;
